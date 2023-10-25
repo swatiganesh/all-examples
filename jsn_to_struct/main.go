0edd1358303f3ea99151ab3b84adff2c167880b3
swatign <SwatiGanesh.Naik@Progress.com>
2023-10-25 15:37:07 +0530

    updated jsn to struct assignment

--- jsn_to_struct/main.go
+++ jsn_to_struct/main.go
@@ -6,61 +6,65 @@ import (
 	"os"
 )
 
-type TokenizerConfig struct {
+type AutocompleteTokenizer struct {
 	TokenChars []string `json:"token_chars"`
 	MinGram    string   `json:"min_gram"`
 	Type       string   `json:"type"`
 	MaxGram    string   `json:"max_gram"`
 }
 
-type AnalyzerConfig struct {
-	Filter    []string        `json:"filter"`
-	Tokenizer TokenizerConfig `json:"tokenizer"`
+type Analyzer struct {
+	Autocomplete               Autocomplete `json:"autocomplete"`
+	AutocompleteVersionNumbers Autocomplete `json:"autocomplete_version_numbers"`
 }
 
-type NormalizerConfig struct {
-	CaseInsensitive struct {
-		Filter     []string      `json:"filter"`
-		Type       string        `json:"type"`
-		CharFilter []interface{} `json:"char_filter"`
-	} `json:"case_insensitive"`
+type Autocomplete struct {
+	Filter    []string `json:"filter"`
+	Tokenizer string   `json:"tokenizer"`
 }
 
-type AnalysisConfig struct {
-	Normalizer NormalizerConfig `json:"normalizer"`
-	Analyzer   AnalyzerConfig   `json:"analyzer"`
-	Tokenizer  TokenizerConfig  `json:"tokenizer"`
+type Normalizer struct {
+	CaseInsensitive CaseInsensitive `json:"case_insensitive"`
+}
+type CaseInsensitive struct {
+	Filter     []string `json:"filter"`
+	Type       string   `json:"type"`
+	CharFilter []string `json:"char_filter"`
 }
 
-type IndexConfig struct {
-	RefreshInterval  string         `json:"refresh_interval"`
-	NumberOfShards   string         `json:"number_of_shards"`
-	ProvidedName     string         `json:"provided_name"`
-	CreationDate     string         `json:"creation_date"`
-	Analysis         AnalysisConfig `json:"analysis"`
-	NumberOfReplicas string         `json:"number_of_replicas"`
-	UUID             string         `json:"uuid"`
-	Version          struct {
-		Created string `json:"created"`
-	} `json:"version"`
+type Analysis struct {
+	Normalizer Normalizer `json:"normalizer"`
+	Analyzer   Analyzer   `json:"analyzer"`
+	Tokenizer  Tokenizer  `json:"tokenizer"`
+}
+type Tokenizer struct {
+	AutocompleteVersionNumberTokenizer AutocompleteTokenizer `json:"autocomplete_version_number_tokenizer"`
+	AutocompleteTokenizer              AutocompleteTokenizer `json:"autocomplete_tokenizer"`
 }
 
-type SettingsConfig struct {
-	Index IndexConfig `json:"index"`
+type Version struct {
+	Created string `json:"created"`
 }
 
-// type Assignment1 struct {
-// 	Comp7S20211122 struct {
-// 		Settings SettingsConfig `json:"settings"`
-// 	} `json:"comp-7-s-2021.11.22"`
-// 	Comp7S20211123 struct {
-// 		Settings SettingsConfig `json:"settings"`
-// 	} `json:"comp-7-s-2021.11.23"`
-// }
+type Index struct {
+	RefreshInterval  string   `json:"refresh_interval"`
+	NumberOfShards   string   `json:"number_of_shards"`
+	ProvidedName     string   `json:"provided_name"`
+	CreationDate     string   `json:"creation_date"`
+	Analysis         Analysis `json:"analysis"`
+	NumberOfReplicas string   `json:"number_of_replicas"`
+	UUID             string   `json:"uuid"`
+	Version          Version  `json:"version"`
+}
+
+type Settings struct {
+	Index Index `json:"index"`
+}
 
 type example struct {
-	Settings SettingsConfig `json:"settings"`
+	Settings Settings `json:"settings"`
 }
+
 type Assignment1 struct {
 	Comp7S20211122 example `json:"comp-7-s-2021.11.22"`
 	Comp7S20211123 example `json:"comp-7-s-2021.11.23"`
@@ -122,10 +126,10 @@ func main() {
 	fmt.Println("Comp7S20211123 Analyzer Tokenizer:", data.Comp7S20211123.Settings.Index.Analysis.Tokenizer)
 	// fmt.Println("Comp7S20211123 UUID:", data.Comp7S20211123.Settings.Index.UUID)
 
-	fmt.Println("Comp7S20211122 Analyzer auto complete version filter:", data.Comp7S20211122.Settings.Index.Analysis.Analyzer.Filter)
+	fmt.Println("Comp7S20211122 Analyzer auto complete version filter:", data.Comp7S20211122.Settings.Index.Analysis.Analyzer)
 
 	fmt.Println("Normalizer Type:", data.Comp7S20211122.Settings.Index.Analysis.Normalizer.CaseInsensitive.Type)
-	fmt.Println("Analyzer Tokenizer Type:", data.Comp7S20211122.Settings.Index.Analysis.Analyzer.Tokenizer.Type)
+	fmt.Println("Analyzer Tokenizer Type:", data.Comp7S20211122.Settings.Index.Analysis.Normalizer.CaseInsensitive.Type)
 
 	// // Accessing values from Comp7S20211123
 	// fmt.Println("Normalizer Type (Comp7S20211123):", data.Comp7S20211123.SettingsConfig.Index.Analysis.Normalizer.CaseInsensitive.Type)
